420426fd3964b72fa164cdeadf3be75517e17629
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-23 11:52:02 -0600

    Add route for searching for rides

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Mechanics
   get '/mechanics', to: 'mechanics#index'
   get '/mechanics/:mechanic_id', to: 'mechanics#show'
+  post '/mechanics/:mechanic_id', to: 'ride_mechanics#create'
   # Rides
   # Parks
   get '/parks/:amusement_park_id', to: 'amusement_parks#show'
